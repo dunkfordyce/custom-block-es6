a3d4a8ee78f5ce0d1341de69d7f3f5bcb92cb62c
Dunk Fordyce <dunkfordyce@gmail.com>
2017-11-17 10:12:49 +0000

    initial

---
+++ build/vue-loader.conf.js
@@ -0,0 +1,25 @@
+'use strict'
+const utils = require('./utils')
+const config = require('../config')
+const isProduction = process.env.NODE_ENV === 'production'
+const sourceMapEnabled = isProduction
+  ? config.build.productionSourceMap
+  : config.dev.cssSourceMap
+
+var loaders = utils.cssLoaders({
+  sourceMap: sourceMapEnabled,
+  extract: isProduction
+})
+
+loaders.foo = require.resolve('./foo-loader.js')
+
+module.exports = {
+  loaders,
+  cssSourceMap: sourceMapEnabled,
+  transformToRequire: {
+    video: 'src',
+    source: 'src',
+    img: 'src',
+    image: 'xlink:href'
+  }
+}
